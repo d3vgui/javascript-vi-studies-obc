8ca75cb6847f63d2e651038c2070eeeb2f87da89
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-07-17 11:40:17 -0300

    exemplo de código aula 6 - utilizando .then() e .catch() em promises

---
+++ aula-6-utilizando-then()-e-catch()-em-promises/script.js
@@ -0,0 +1,17 @@
+function execute() {
+    return new Promise((resolve, reject) => {
+        console.log('A promise está sendo executada.')
+        setTimeout(() => {
+            console.log('Resolvendo a promise...')
+            resolve(42)
+        }, 3 * 1000)
+    })
+}
+
+execute().then((result) => {
+    console.log(`A promise foi resolvida. O resultado foi: ${result}`)
+}).catch((err) => {
+    console.log(`A promise foi rejeitada! Motivo: ${err}`)
+}).finally(() => {
+    console.log('A promise foi finalizada')
+})
